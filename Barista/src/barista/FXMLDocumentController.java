a0dbab5e303355b511e875780e12bd57c0b24fda
Arik Poznanski <arik.com@gmail.com>
2014-09-26 19:57:28 +0300

    added basic handler for selecting project folder

--- Barista/src/barista/FXMLDocumentController.java
+++ Barista/src/barista/FXMLDocumentController.java
@@ -5,12 +5,18 @@
  */
 package barista;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuBar;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Window;
 
 /**
  *
@@ -18,13 +24,28 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
+    @FXML MenuBar menuBar;
+    
     @FXML
     private Label label;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void handleExitAction(ActionEvent event) {
+        Platform.exit();
+    }
+    
+    @FXML
+    private void handleOpenProjectAction(ActionEvent event) {
+        // select project directory
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        Window currentWindow = menuBar.getScene().getWindow();
+        File selectedDirectory = directoryChooser.showDialog(currentWindow);
+        
+        if (selectedDirectory != null)
+        {
+            // use selected project directory
+            System.out.println(selectedDirectory.getAbsolutePath());
+        }
     }
     
     @Override
