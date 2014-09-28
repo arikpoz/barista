2d9bdb553798316449d4101685ec5f545f2c0ff3
Arik Poznanski <arik.com@gmail.com>
2014-09-28 22:44:38 +0300

    updated proto definition of application.settings

--- Barista/proto/compile_proto.bat
+++ Barista/proto/compile_proto.bat
@@ -1,1 +1,1 @@
-..\..\ThirdParty\protobuf-2.6.0\bin\protoc --java_out=../src/barista -I../../ThirdParty/protobuf-2.6.0/src/google/protobuf/ -I./ ./barista_messages.proto
+..\..\ThirdParty\protobuf-2.6.0\src\protoc --java_out=../src -I../../ThirdParty/protobuf-2.6.0/src/google/protobuf/ -I./ ./barista_messages.proto
