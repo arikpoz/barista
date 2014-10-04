203f4d5b2f0e650c52911c8667a547ac38a92179
Arik Poznanski <arik.com@gmail.com>
2014-10-05 01:52:02 +0300

    added ProtobufProperty and ProtobufPropertyList to model added recursive code to build ProtobufPropertyList from protobuf GeneratedMessage

---
+++ Barista/src/barista/model/ProtobufProperty.java
@@ -0,0 +1,159 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package barista.model;
+
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ *
+ * @author Arik
+ */
+public class ProtobufProperty {
+
+    // <editor-fold desc="name property" defaultstate="collapsed">
+    private final StringProperty name = new SimpleStringProperty();
+
+    public final String getName() {
+        return name.get();
+    }
+
+    public final void setName(String value) {
+        this.name.set(value);
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+    // </editor-fold>
+
+    // <editor-fold desc="type property" defaultstate="collapsed">
+    private final StringProperty type = new SimpleStringProperty();
+
+    public final String getType() {
+        return type.get();
+    }
+
+    public final void setType(String value) {
+        this.type.set(value);
+    }
+
+    public StringProperty typeProperty() {
+        return type;
+    }
+    // </editor-fold>
+
+    // <editor-fold desc="hasValue property" defaultstate="collapsed">
+    private final BooleanProperty hasValue = new SimpleBooleanProperty();
+
+    public final Boolean getHasValue() {
+        return hasValue.get();
+    }
+
+    public final void setHasValue(Boolean value) {
+        this.hasValue.set(value);
+    }
+
+    public BooleanProperty hasValueProperty() {
+        return hasValue;
+    }
+    // </editor-fold>
+
+    // <editor-fold desc="value property" defaultstate="collapsed">
+    private final ObjectProperty value = new SimpleObjectProperty();
+
+    public final Object getValue() {
+        return value.get();
+    }
+
+    public final void setValue(Object value) {
+        this.value.set(value);
+    }
+
+    public ObjectProperty valueProperty() {
+        return value;
+    }
+    // </editor-fold>
+
+    // <editor-fold desc="isMessage property" defaultstate="collapsed">
+    private final BooleanProperty isMessage = new SimpleBooleanProperty();
+
+    public final Boolean getIsMessage() {
+        return isMessage.get();
+    }
+
+    public final void setIsMessage(Boolean value) {
+        this.isMessage.set(value);
+    }
+
+    public BooleanProperty isMessageProperty() {
+        return isMessage;
+    }
+    // </editor-fold>
+    
+    // <editor-fold desc="isOptional property" defaultstate="collapsed">
+    private final BooleanProperty isOptional = new SimpleBooleanProperty();
+
+    public final Boolean getIsOptional() {
+        return isOptional.get();
+    }
+
+    public final void setIsOptional(Boolean value) {
+        this.isOptional.set(value);
+    }
+
+    public BooleanProperty isOptionalProperty() {
+        return isOptional;
+    }
+    // </editor-fold>
+
+    // <editor-fold desc="isRepeated property" defaultstate="collapsed">
+    private final BooleanProperty isRepeated = new SimpleBooleanProperty();
+
+    public final Boolean getIsRepeated() {
+        return isRepeated.get();
+    }
+
+    public final void setIsRepeated(Boolean value) {
+        this.isRepeated.set(value);
+    }
+
+    public BooleanProperty isRepeatedProperty() {
+        return isRepeated;
+    }
+    // </editor-fold>
+
+    /**
+     * Intended only for debugging.
+     *
+     * <P>
+     * Here, the contents of every field are placed into the result, with one
+     * field per line.
+     * @return 
+     */
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        String NEW_LINE = System.getProperty("line.separator");
+
+        result.append(this.getClass().getName()).append(" Object {").append(NEW_LINE);
+        result.append(" Name: ").append(getName()).append(NEW_LINE);
+        result.append(" Type: ").append(getType()).append(NEW_LINE);
+        result.append(" HasValue: ").append(getHasValue()).append(NEW_LINE);
+        result.append(" Value: ").append(getValue()).append(NEW_LINE);
+        result.append(" IsMessage: ").append(getIsMessage()).append(NEW_LINE);
+        result.append(" IsOptional: ").append(getIsOptional()).append(NEW_LINE);
+        result.append(" IsRepeated: ").append(getIsRepeated()).append(NEW_LINE);
+        result.append("}");
+
+        return result.toString();
+    }
+
+}
--- Barista/src/barista/view/ProjectOverviewController.java
+++ Barista/src/barista/view/ProjectOverviewController.java
@@ -8,9 +8,14 @@ package barista.view;
 import barista.BaristaMessages;
 import barista.MainApp;
 import barista.model.Configuration;
+import barista.model.ProtobufProperty;
+import barista.model.ProtobufPropertyList;
 import caffe.Caffe;
 import caffe.Caffe.NetParameter;
 import caffe.Caffe.SolverParameter;
+import com.google.protobuf.Descriptors.Descriptor;
+import com.google.protobuf.Descriptors.FieldDescriptor;
+import com.google.protobuf.GeneratedMessage;
 import com.google.protobuf.TextFormat;
 import java.io.File;
 import java.io.FileWriter;
@@ -18,9 +23,13 @@ import java.io.FilenameFilter;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.Observable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.beans.binding.Bindings;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -28,6 +37,9 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.TreeTableView;
 
 /**
  *
@@ -51,10 +63,24 @@ public class ProjectOverviewController {
     private TableColumn<Configuration, String> nameColumn;
 
     @FXML
-    private TextField nameTextField;
+    private TreeTableView solverTreeTableView;
 
     @FXML
-    private TextField solverFileNameTextField;
+    private TreeTableColumn nameSolverColumn;
+
+    @FXML
+    private TreeTableColumn typeSolverColumn;
+
+    @FXML
+    private TreeTableColumn valueSolverColumn;
+
+    private TreeItem solverRootItem;
+
+    @FXML
+    private TreeTableView trainTreeTableView;
+
+    @FXML
+    private TreeTableView testTreeTableView;
 
     // Reference to the main application.
     private MainApp mainApp;
@@ -73,7 +99,7 @@ public class ProjectOverviewController {
     @FXML
     private void initialize() {
 
-        // initialize the configuration table 
+        // initialize the configuration table columns
         nameColumn.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
 
         // clear configuration details
@@ -82,6 +108,9 @@ public class ProjectOverviewController {
         // listen for selection changes and show the configuration details when changed
         configurationTable.getSelectionModel().selectedItemProperty().addListener(
                 (observable, oldValue, newValue) -> showConfigurationDetails(newValue));
+
+        solverRootItem = new TreeItem();
+        solverTreeTableView.setRoot(solverRootItem);
     }
 
     /**
@@ -125,40 +154,6 @@ public class ProjectOverviewController {
         }
     }
 
-    /**
-     * Fills all text fields to show details about the configuration. If the
-     * specified configuration is null, all text fields are cleared.
-     *
-     * @param configuration the configuration or null
-     */
-    private void showConfigurationDetails(Configuration configuration) {
-        if (configuration != null) {
-            // Fill the labels with info from the configuration object.
-
-            // find solver file name
-            File solverFile = findSolverFile(configuration.getName());
-            if (solverFile != null) {
-                
-                // build solver object
-                SolverParameter solverParameter = mainApp.readSolverParameter(solverFile.getAbsolutePath());
-
-                Path trainFilePath = FileSystems.getDefault().getPath(mainApp.getProjectFolder(), configuration.getName(), solverParameter.getTrainNet());
-                NetParameter trainNetParameter = mainApp.readNetParameter(trainFilePath.toString());
-
-                Path testFilePath = FileSystems.getDefault().getPath(mainApp.getProjectFolder(), configuration.getName(), solverParameter.getTestNet());
-                NetParameter testNetParameter = mainApp.readNetParameter(testFilePath.toString());
-
-                // TODO remove when debugging is over
-                System.out.println(solverParameter.toString());
-                System.out.println(trainNetParameter.toString());
-                System.out.println(testNetParameter.toString());
-            }
-
-        } else {
-            // configuration is null
-        }
-    }
-
     private File findSolverFile(String configurationName) {
 
         Path configurationFilePath = FileSystems.getDefault().getPath(mainApp.getProjectFolder(), configurationName);
@@ -182,5 +177,114 @@ public class ProjectOverviewController {
         } else {
             return files[0];
         }
+    }
+
+    /**
+     * Fills all text fields to show details about the configuration. If the
+     * specified configuration is null, all text fields are cleared.
+     *
+     * @param configuration the configuration or null
+     */
+    private void showConfigurationDetails(Configuration configuration) {
+        if (configuration != null) {
+            // Fill the labels with info from the configuration object.
+
+            // find solver file name
+            File solverFile = findSolverFile(configuration.getName());
+            if (solverFile != null) {
+
+                // build solver object
+                SolverParameter solverParameter = mainApp.readSolverParameter(solverFile.getAbsolutePath());
+
+                Path trainFilePath = FileSystems.getDefault().getPath(mainApp.getProjectFolder(), configuration.getName(), solverParameter.getTrainNet());
+                NetParameter trainNetParameter = mainApp.readNetParameter(trainFilePath.toString());
+
+                Path testFilePath = FileSystems.getDefault().getPath(mainApp.getProjectFolder(), configuration.getName(), solverParameter.getTestNet());
+                NetParameter testNetParameter = mainApp.readNetParameter(testFilePath.toString());
+
+                ProtobufPropertyList solverPropertyList = loadProtoObjectToProtoPropertyList(solverParameter);
+                ProtobufPropertyList trainPropertyList = loadProtoObjectToProtoPropertyList(trainNetParameter);
+                ProtobufPropertyList testPropertyList = loadProtoObjectToProtoPropertyList(testNetParameter);
+
+                // TODO remove when debugging is over
+                //System.out.println(solverParameter.toString());
+                //System.out.println(trainNetParameter.toString());
+                //System.out.println(testNetParameter.toString());
+            }
+
+        } else {
+            // configuration is null
+        }
+    }
+
+    private ProtobufPropertyList loadProtoObjectToProtoPropertyList(GeneratedMessage protobufMessage) {
+
+        ProtobufPropertyList protobufPropertyList = new ProtobufPropertyList();
+
+        Descriptor descriptor = protobufMessage.getDescriptorForType();
+
+        List<FieldDescriptor> fieldsDescriptors = descriptor.getFields();
+
+        for (FieldDescriptor fieldDescriptor : fieldsDescriptors) {
+
+            ProtobufProperty protobufProperty = new ProtobufProperty();
+            protobufProperty.setName(fieldDescriptor.getName());
+            protobufProperty.setType(fieldDescriptor.getType().name());
+
+            if (protobufProperty.getType() == "MESSAGE") {
+                protobufProperty.setIsMessage(true);
+                protobufProperty.setType(fieldDescriptor.getMessageType().getName());
+            } else {
+                protobufProperty.setIsMessage(false);
+            }
+
+            protobufProperty.setIsOptional(fieldDescriptor.isOptional());
+            protobufProperty.setIsRepeated(fieldDescriptor.isRepeated());
+
+            protobufProperty.setHasValue((protobufProperty.getIsOptional()) && (protobufMessage.hasField(fieldDescriptor)));
+
+            // handle repeated message
+            if ((protobufProperty.getIsMessage()) && (protobufProperty.getIsRepeated())) {
+                ObservableList<ProtobufPropertyList> objectsList = FXCollections.observableArrayList();
+
+                // for each item
+                int count = protobufMessage.getRepeatedFieldCount(fieldDescriptor);
+                for (int i = 0; i < count; i++) {
+                    GeneratedMessage currentMessageItem = (GeneratedMessage) protobufMessage.getRepeatedField(fieldDescriptor, i);
+                    ProtobufPropertyList currentObject = loadProtoObjectToProtoPropertyList(currentMessageItem);
+                    objectsList.add(currentObject);
+                }
+
+                protobufProperty.setValue(objectsList);
+
+            } else if (protobufProperty.getIsRepeated()) {
+                // handle repeated
+                ObservableList<Object> objectsList = FXCollections.observableArrayList();
+
+                // for each item
+                int count = protobufMessage.getRepeatedFieldCount(fieldDescriptor);
+                for (int i = 0; i < count; i++) {
+                    Object currentItem = protobufMessage.getRepeatedField(fieldDescriptor, i);
+                    objectsList.add(currentItem);
+                }
+
+                protobufProperty.setValue(objectsList);
+
+            } else if (protobufProperty.getIsMessage()) {
+                // handle message
+                GeneratedMessage singleMessageItem = (GeneratedMessage) protobufMessage.getField(fieldDescriptor);
+                ProtobufPropertyList singleObject = loadProtoObjectToProtoPropertyList(singleMessageItem);
+                protobufProperty.setValue(singleObject);
+            } else if (protobufProperty.getHasValue()) {
+                protobufProperty.setValue(protobufMessage.getField(fieldDescriptor));
+            } else if (fieldDescriptor.hasDefaultValue()) {
+                protobufProperty.setValue(fieldDescriptor.getDefaultValue());
+            }
+
+            //System.out.println(protobufProperty);
+            protobufPropertyList.getProtobufPropertyList().add(protobufProperty);
+        }
+
+        return protobufPropertyList;
     }
 }
